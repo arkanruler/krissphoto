d0f333de995e831bf2e1e97625622813cbee7018
arkanruler <sktparting@gmail.com>
2020-10-18 13:55:48 +0400

    krisswithoutgallery

---
+++ src/components/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <router-link class="nav-link active" to="/">Home </router-link>
+      <router-link class="nav-link" to="/gallery">Gallery</router-link>
+      <router-link class="nav-link" to="/contact">Contacts</router-link>
+    </div>
+  </div>
+</nav>
+</template>
+
+
+
+<style>
+.navbar
+{
+position: fixed;
+top: 0;
+left: 0;
+right: 0; 
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="fon">
+<div class="home">   
+      <h1 class="text">Kristina Baydaricko <p>Master of portrait photography</p></h1>
   </div>
+</div>
+
+
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { TimelineMax, TweenMax, } from "gsap";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+export default{
+  mounted() {
+    
+
+    let tl = new TimelineMax()
+    // tl.fromTo(App, 1, {y:1000}, {y:0})
+    let els = document.querySelectorAll(".js-splitme");
+[].forEach.call(els, function(el)
+{  
+    // el.outerHTML = Splitter(el.outerHTML, '<span class="letter">$</span>');
+    
+});
+    //  tl.fromTo(".fon", 1, {y:1200}, {y:0})
+    tl.from(".fon",0.85,{autoAlpha:0})
+    tl.from("h1",0.47,{autoAlpha:0})
+    
+    
+  
+  }
+} 
+
 </script>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Piedra&display=swap');
+// .navbar
+// {
+// position: fixed;
+// top: 0;
+// left: 0;
+// right: 0;
+//}
+.home{
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+min-height: 667px;
+position: relative;
+background: rgba(0, 0, 0, 0.582);
+opacity: 1;
+}
+.fon {
+position:absolute;
+bottom: 100px;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background-image: url(../assets/main.jpg);
+background-repeat: no-repeat;
+background-size:100%;
+ z-index: -1; 
+}
+.text
+{
+  display:initial;
+  font-family: 'Piedra', cursive;
+  color:#ffffff;
+  padding-bottom: 500px;
+  padding-right: 1001px;
+  font-size: 50px;
+  font-weight: 100px;
+
+}
+
+</style>
